f7502614ae685fc301c2c643e6724428d6022ce1
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-06 13:45:06 +0300

    Swiper Slider in the Clients Section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-const mySwiper = new Swiper('.offer-slider-container', {
-  slidesPerView: 'auto',
-  loop: true,
-  grabCursor: true
-})
+(function () {
+  const offerSlider = new Swiper('.offer-slider-container', {
+    slidesPerView: 'auto',
+    loop: true,
+    grabCursor: true
+  })
+
+  const clientsSlider = new Swiper('.clients-container', {
+    slidesPerView: 'auto',
+    loop: true,
+    grabCursor: true
+  })
+}());
